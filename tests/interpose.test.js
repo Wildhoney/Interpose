1b3bfdffe461c4fa26ba5b00cb1c7ba8605bf1ec
Wildhoney <adam.timberlake@gmail.com>
2016-07-28 12:44:17 +0100

    Updated tests for passed component props

--- tests/interpose.test.js
+++ tests/interpose.test.js
@@ -19,11 +19,11 @@ const mountFromProps = (nodeAttrs = {}, isRoot = false) => {
 
 	const Component = props => (
 		<StyleProperties map={props.map} isRoot={isRoot}>
-			<h1 {...nodeAttrs}><span>Voila!</span></h1>
+			<h1 {...nodeAttrs}><span>{props.greeting}</span></h1>
 		</StyleProperties>
 	);
 
-	const wrapper = mount(<Component map={map} />);
+	const wrapper = mount(<Component map={map} greeting="Hello!" />);
 
 	return {
 		map,
@@ -40,34 +40,37 @@ test('It should be able to render the CSS map and modify the values;', t => {
 	const { header, styles, map, wrapper, propsToStyles } = mountFromProps();
 
 	t.is(styles.text(), `h1 { ${propsToStyles(map)} }`);
-	t.is(header.text(), 'Voila!');
+	t.is(header.text(), 'Hello!');
 
 	const updatedMap = { ...map, colour: 'green' };
 	wrapper.setProps({ map: updatedMap });
 	t.is(styles.text(), `h1 { ${propsToStyles(updatedMap)} }`);
+
+	wrapper.setProps({ greeting: 'Bonjour!' });
+	t.is(header.text(), 'Bonjour!');
 
 });
 
 test('It should be able to render the CSS map for a root component;', t => {
 	const { header, styles, map, propsToStyles } = mountFromProps({ className: 'test' }, true);
 	t.is(styles.text(), `:root { ${propsToStyles(map)} }`);
-	t.is(header.text(), 'Voila!');
+	t.is(header.text(), 'Hello!');
 });
 
 test('It should be able to render the CSS map for a class component;', t => {
 	const { header, styles, map, propsToStyles } = mountFromProps({ className: 'test' });
 	t.is(styles.text(), `h1.test { ${propsToStyles(map)} }`);
-	t.is(header.text(), 'Voila!');
+	t.is(header.text(), 'Hello!');
 });
 
 test('It should be able to render the CSS map for an ID component;', t => {
 	const { header, styles, map, propsToStyles } = mountFromProps({ id: 'test' });
 	t.is(styles.text(), `h1#test { ${propsToStyles(map)} }`);
-	t.is(header.text(), 'Voila!');
+	t.is(header.text(), 'Hello!');
 });
 
 test('It should be able to render the CSS map for a class and ID component;', t => {
 	const { header, styles, map, propsToStyles } = mountFromProps({ id: 'test-id', className: 'test-class' });
 	t.is(styles.text(), `h1#test-id.test-class { ${propsToStyles(map)} }`);
-	t.is(header.text(), 'Voila!');
+	t.is(header.text(), 'Hello!');
 });
